6c09f14c886e04d8d0022bdde868a7a0af60a462
electricprogramming <yeungj316@gmail.com>
2025-06-20 11:32:34 -0400

    olaena dex

--- src/fakemon-list.js
+++ src/fakemon-list.js
@@ -44,6 +44,56 @@ const fakemonList = {
         meaning: 'species of whale known for its tusk'
       }
     ]
+  },
+  olaena: {
+    basic: {
+      name: 'Olaena',
+      number: '008',
+      type: ['Water'],
+      species: 'Narwhal',
+      height: 1.2,
+      weight: 13.5,
+      ability: {
+        normal: ['Torrent'],
+        hidden: 'Swift Swim'
+      }
+    },
+    training: {
+      ev_yield: { sp_atk: 1, speed: 1 },
+      catch_rate: 45,
+      growth_rate: 'Medium Slow'
+    },
+    breeding: {
+      egg_groups: ['Water-2'],
+      gender: { male: 0.5, female: 0.5 },
+      egg_cycles: 20
+    },
+    base_stats: {
+      hp: 80,
+      attack: 45,
+      defense: 60,
+      sp_atk: 80,
+      sp_def: 60,
+      speed: 75
+    },
+    evo: [
+      'Narwhali',
+      'Level 16',
+      'Olaena',
+      'Level 36',
+      'Narquarius'
+    ],
+    dex_entry: `Olaena glides through arctic waters with elegance and grace. Its tusk has fully developed into a spiraled horn capable of sensing even the faintest vibrations in the sea. Olaena communicate with each other using complex series of clicks and whistles.`,
+    name_origin: [
+      {
+        word: 'ola',
+        meaning: 'Spanish word for wave'
+      },
+      {
+        word: 'ballena',
+        meaning: 'Spanish word for whale'
+      }
+    ]
   }
 };
 export default fakemonList
