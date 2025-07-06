dfde1756acf470fe83114b057384a8ad87c633ce
electricprogramming <yeungj316@gmail.com>
2025-07-06 15:34:13 -0400

    hm

--- src/fakemon-list.js
+++ src/fakemon-list.js
@@ -12,7 +12,7 @@ const fakemonList = {
         hidden: 'Inner Focus'
       }
     },
-    other_info: `Bambao is one of three First Partner Pokémon available in the -- region, along with -- and <a class="basic-link" href="/dex/narwhali">Narwhali</a>.`,
+    other_info: `Bambao is one of three First Partner Pokémon available in the Jinlu region, along with -- and <a class="basic-link" href="/dex/narwhali">Narwhali</a>.`,
     training: {
       ev_yield: { attack: 1 },
       catch_rate: 45,
@@ -69,7 +69,7 @@ const fakemonList = {
         hidden: 'Swift Swim'
       }
     },
-    other_info: `Narwhali is one of three First Partner Pokémon available in the -- region, along with <a class="basic-link" href="/dex/bambao">Bambao</a> and --.`,
+    other_info: `Narwhali is one of three First Partner Pokémon available in the Jinlu region, along with <a class="basic-link" href="/dex/bambao">Bambao</a> and --.`,
     training: {
       ev_yield: { speed: 1 },
       catch_rate: 45,
