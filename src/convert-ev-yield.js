51eedff1721a2f79a67b4b1beacc0b1efa58b7de
electricprogramming <yeungj316@gmail.com>
2025-07-07 15:22:03 -0400

    sort

--- src/convert-ev-yield.js
+++ src/convert-ev-yield.js
@@ -1,6 +1,8 @@
 import { convertStatNameForEvYield } from './convert-stat-name.js';
+const order = ['hp', 'attack', 'defense', 'sp_atk', 'sp_def', 'speed'];
 export default function evYieldAsString(obj) {
   return Object.entries(obj)
+    .sort((a, b) => (a[1] - b[1]) || (order.indexOf(a[0]) - order.indexOf(b[0])))
     .map(([key, val]) => `${val} ${convertStatNameForEvYield(key)}`)
     .join(', ');
 }
